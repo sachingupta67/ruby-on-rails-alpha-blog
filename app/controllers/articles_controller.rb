eac70ae29c5ecc1c41a2d4ae2e043338fc1d8d3b
Sachin Gupta <sachin.gupta@peason.com>
2025-03-18 20:59:52 +0530

    CRUD - View List

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
-    # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+    def index
+        @articles = Article.all # fetch all articles from the database and assign to @articles variable.
+    end
+   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
    def show
     @article = Article.find(params[:id]) # article/1 , :id `params[:id]` # @ to make it instance to access in the template
    end
-  end
-  
+end
