f55b3255ea14ef710e9ec0600ff9a5d8762d32f0
Sachin Gupta <sachin.gupta@peason.com>
2025-03-19 00:05:31 +0530

    CRUD - Create Operation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,17 @@ class ArticlesController < ApplicationController
    def show
     @article = Article.find(params[:id]) # article/1 , :id `params[:id]` # @ to make it instance to access in the template
    end
+
+   def create
+     #  @article = Article.new(params[:article]) it will not work , give forbiden error message
+
+     @article = Article.new(params.require(:article).permit(:title, :description))
+     @article.save
+
+     #  redirect_to article_path(@article.id)
+     redirect_to @article
+   end
+
+   def new
+   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: {minimum:6 , maximum:10}
-    validates :description, presence: true, length: {minimum:6 , maximum:10}
+    validates :title, presence: true, length: { minimum: 6, maximum: 10 }
+    validates :description, presence: true, length: { minimum: 6, maximum: 10 }
 end
